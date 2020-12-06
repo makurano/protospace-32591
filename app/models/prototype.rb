d3478733d4b37361beb78938a14200618e05c2b8
makurano <ginsyari1@gmail.com>
2020-12-06 21:50:16 +0900

    コメント投稿機能実装

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -4,6 +4,7 @@ class Prototype < ApplicationRecord
   validates :concept,    presence: true
   validates :image,      presence: true
   
+  has_many :comments, dependent: :destroy
   belongs_to :user
   has_one_attached :image
 end
